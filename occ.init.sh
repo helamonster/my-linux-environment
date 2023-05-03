e4cf269363d6613dabcea049997e4a2085aa2202
Jeremy Bryan Smith <helamonster@gmail.com>
2023-05-03 15:28:56 -0400

    Update occ.init.sh

--- occ.init.sh
+++ occ.init.sh
@@ -8,9 +8,12 @@ echo "Ext ip: $EXT_IP"
 
 sudo apt-get update
 
-sudo apt-get install -y ssh tmux puppet vim git openvpn lastpass-cli nmap aptitude sshfs snapd
+sudo apt-get install -y ssh tmux puppet vim git lastpass-cli nmap aptitude sshfs snapd
 
 sudo apt-get dist-upgrade -y
+
+# vpn
+sudo apt-get install -y openvpn openvpn-systemd-resolved
 
 # multimedia
 # sudo apt-get install -y mplayer ffmpeg vlc
@@ -21,6 +24,9 @@ sudo apt-get dist-upgrade -y
 
 # snap packages
 sudo snap install brave
+
+# for firefox nightly
+sudo apt-get install libdbus-glib-1-2
 
 
 # puppet ....
